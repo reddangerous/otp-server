e404bfd0b19bd6f878fb726f388dbbc5af43c7da
reddangerous <dkinyanjui144@gmail.com>
2023-07-20 16:55:02 +0300

    u

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //require('dotenv').config();
-const express = require('express');
+//const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const app = express();
